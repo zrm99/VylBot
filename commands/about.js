8343272d14f685d9e881df04cb0d4b22cd6c52a4
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2020-04-20 18:24:27 +0200

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -5,6 +5,12 @@ var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
 
 exports.run = function(message, prefix, args) {
+	if(config.disabledCommands.includes('about')){
+		message.delete();
+		if(config.commands.sendMessageIfOff == true){
+			functions.embed(message.channel, "Whoops", colourInfo,"It seems that this Command is disabled!\n If you belive that this is an error,\n contact the bot Owner!");
+		}
+	}else{
 	var commands = "";
 	
 	commands += "Version: " + config.info.version + "\n";
@@ -12,4 +18,5 @@ exports.run = function(message, prefix, args) {
 	commands += "Date: " + config.info.date + "\n";
 	
 	functions.embed(message.channel, "About", colourInfo, commands);
+	}
 }
