38bb4d494d84d4facba4d2ffe1586a27e0d95cb6
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2020-04-21 15:41:35 +0200

    Fixed: variable names

--- commands/poll.js
+++ commands/poll.js
@@ -13,8 +13,8 @@ exports.run = function(message, prefix, args) {
     //checks for permissions because thats how it works right?
     if (message.member.roles.find(role => role.name == config.roles.moderator)) {
         //putting the options into a var to use later
-        var option_one = args[0];
-        var option_two = args[1];
+        var optionOne = args[0];
+        var optionTwo = args[1];
         //putting the title into a var with a fancy loop
         var temp = 2;
         var title
@@ -24,32 +24,32 @@ exports.run = function(message, prefix, args) {
         //actully sending the message
         functions.embed(config.poll,title,colourInfo, ":one:" + option_one + "\n"+ option_two)
         //putting the last message(the poll) into a var for later use
-        var last_message = message.channel.last_message;
+        var lastMessage = message.channel.last_message;
         //reacting with the vote options
-        last_message.react("👍");
-        last_message.react("👎");
+        lastMessage.react("👍");
+        lastMessage.react("👎");
     }else {
         //sending the: "no permissions" message
         functions.embed(message.channel, "", colourWarn, "You do not have permission to run this command");
     }
    }if(args.length === 0){
-    if(agrs[0] == "end"){
+    if(args[0] == "end"){
             //deleting the sender message for the reaction collection thing...
             message.delete();
             //setting up stuff
-            var last_message = message.channel.last_message;
+            var lastMessage = message.channel.last_message;
             //collection 1
             const filter = (reaction) => reaction.emoji.name === '👍';
             const collector = message.createReactionCollector(filter);
-            var option_end_one 
-            collector.on('end', collected => option_end_one = `${collected.size}`);
+            var optionEndOne 
+            collector.on('end', collected => optionEndOne = `${collected.size}`);
             //collection 2
             const filter2 = (reaction) => reaction.emoji.name === '👎';
             const collector2 = message.createReactionCollector(filter2);
-            var option_end_two 
-            collector2.on('end', collected => option_end_two = `${collected.size}`);
+            var optionEndTwo 
+            collector2.on('end', collected => optionEndTwo = `${collected.size}`);
             //sending the result
-            functions.embed(message.channel, "The Poll has ended:", colourInfo, option_end_one + ` have chosen the first option. \n` + option_end_two + " have chosen the second option.");
+            functions.embed(message.channel, "The Poll has ended:", colourInfo, optionEndOne + ` have chosen the first option. \n` + optionEndTwo + " have chosen the second option.");
     }
    }else{
        //sends a message for all the people not knowing how to make a poll... Idiots...
