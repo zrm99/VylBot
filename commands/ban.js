87a217b53f9a70d8579868d86224f62389eb6dfa
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2020-04-22 18:32:39 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -11,12 +11,6 @@ exports.run = function(message, prefix, args) {
 			functions.embed(message.channel, "Whoops", colourInfo,"It seems that this Command is disabled!\n If you belive that this is an error,\n contact the bot Owner!");
 		}
 	}else{
-	if(config.disabledCommands.includes('ban')){
-		message.delete();
-		if(config.commands.sendMessageIfOff == true){
-			functions.embed(message.channel, "Whoops", colourInfo,"It seems that this Command is disabled!\n If you belive that this is an error,\n contact the bot Owner!");
-		}
-	}else{
 	if (message.member.roles.find(role => role.name == config.roles.moderator)) {
 	    var user = message.mentions.users.first();
 	
