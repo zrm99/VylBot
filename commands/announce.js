46bde5c651a33eb6f37349ba09dee36c36f71a0f
Ethan Lane <elane2501@gmail.com>
2020-04-25 22:34:50 +0100

    Extra boxes

--- commands/announce.js
+++ commands/announce.js
@@ -12,6 +12,11 @@ exports.run = function(message, prefix, args) {
         announcementParts = announcementParts.split(";");
         
         functions.embed(message.guild.channels.find(channel => channel.name == config.channels.announcements), announcementParts[0], colourInfo, announcementParts[1]);
+
+        for (let i = 2; i < announcementParts.length; i++) {
+            functions.embed(message.guild.channels.find(channel => channel.name == config.channels.announcements), "", colourInfo, announcementParts[i]);
+        }
+
         message.delete();
     } else {
         functions.embed(message.channel, "", colourWarn, "You do not have permission to run this command");
