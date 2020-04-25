4c0ef31d3bd950890db06aa1119016d2405cf8ce
Ethan Lane <elane2501@gmail.com>
2020-04-25 22:44:52 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ exports.run = function(message, prefix, args) {
 	var commands = "";
 	
 	commands += prefix + "about\n";
+	commands += prefix + "announce <title>;<text>;[extra]";
 	commands += prefix + "ban <user> [reason]\n";
 	commands += prefix + "help\n";
 	commands += prefix + "kick <user> [reason]\n";
