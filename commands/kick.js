9b3dd17de6ce16922a04678f7d487be1b0e9673b
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2020-04-28 20:11:03 +0200

    Fixed everything
    Fixed Ban,kick,mute

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,14 @@
 const config = require('../config.json');
 const functions = require('../functions.js');
+const discord = require(`discord.js`)
 
 var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
 
 exports.run = function(message, prefix, args) {
 	if (message.member.roles.find(role => role.name == config.roles.moderator)) {
-	    var user = message.mentions.users.first();
+		var user = message.mentions.users.first();
+		var name = message.guild.name;
 	
 	    if (user) {
 	        var member = message.guild.member(user);
@@ -15,16 +17,26 @@ exports.run = function(message, prefix, args) {
 	            var argsReason = args;
 	            argsReason.splice(0, 2);
 	            
-	            var reason = argsReason.join(" ");
-	
-	            member.kick(reason).then(() => {
-			user.sendMessage(user,"You have been kicked from **" + guild.name + "** for the reason:" + reason);
+				var reason = argsReason.join(" ");
+				if(message.guild.available){
+				var embed = new discord.RichEmbed()
+						.setTitle(`You have been kicked from: ${name}`)
+						.setColor(colourWarn)
+						.setDescription(`For the reason: ${reason}`);
+				member.send(embed).then(()=>{
+	            	member.kick(reason).then(() => {
 	                functions.embed(message.channel, "", colourInfo, user.tag + " has been kicked");
 	                functions.embed(message.guild.channels.find(channel => channel.name == config.channels.logging), "Member Kicked", colourInfo, "Member: " + user.tag + "\n Reason: " + reason + "\n Moderator: " + message.author.tag);
 	            }).catch(err => {
 					functions.embed(message.channel, "", colourWarn, "There was an error kicking this user, maybe I'm missing permissions?");
 					console.log(err);
-	            });
+				});
+			}).catch(() => {
+				functions.embed(message.guild.channels.find(channel => channel.name == config.channels.logging), "Error(While DMing user)", colourWarn,"An error occurred while DMing a user.\nIs the user in this Server?")
+			});
+			}else{
+
+			}
 	        } else {
 	            functions.embed(message.channel, "", colourWarn, "This user is not in the server");
 	        }
--- commands/mute.js
+++ commands/mute.js
@@ -1,12 +1,14 @@
 const config = require('../config.json');
 const functions = require('../functions.js');
+const discord = require('discord.js');
 
 var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
 
 exports.run = function(message, prefix, args) {
 	if (message.member.roles.find(role => role.name == config.roles.moderator)) {
-	    var user = message.mentions.users.first();
+		var user = message.mentions.users.first();
+		var name = message.guild.name;
 	
 	    if (user) {
 	        var member = message.guild.member(user);
@@ -16,17 +18,26 @@ exports.run = function(message, prefix, args) {
 	            argsReason.splice(0, 2);
 	
 	            var reason = argsReason.join(" ");
-	
+				if(message.guild.available){
 	            var mutedRole = message.guild.roles.find(role => role.name == config.roles.muted);
-				
+				var embed = new discord.RichEmbed()
+						.setTitle(`You were muted from: ${name}`)
+						.setColor(colourWarn)
+						.setDescription(`For the reason: ${reason}`)
 	            member.addRole(mutedRole).then(() => {
-			user.sendMessage(user,"You have been muted for the reason:" + reason);
+					member.send(embed).then(()=>{
 	                functions.embed(message.channel, "", colourInfo, user.tag + " has been muted");
 	                functions.embed(message.guild.channels.find(channel => channel.name == config.channels.logging), "Member Muted", colourInfo, "Member: " + user.tag + "\n Reason: " + reason + "\n Moderator: " + message.author.tag)
-	            }).catch(err => {
+				}).catch(() => {
+					functions.embed(message.guild.channels.find(channel => channel.name == config.channels.logging), "Error(While DMing user)", colourWarn, "An error occurred while DMing a user.\n Is the user in this Server?")
+				});
+				}).catch(err => {
 					functions.embed(message.channel, "", colourWarn, "There was an error muting this user, maybe I'm missing permissions?");
 					console.log(err);
-	            });
+				});
+			}else{
+				functions.embed(message.channel, "Err", colourWarn, "An unkown error occured")
+			}
 	        } else {
 	            functions.embed(message.channel, "", colourWarn, "This user is not in the server");
 	        }
