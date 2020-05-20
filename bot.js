50e813bf31a08c18ae8e955490b45198f918bc3d
Simyon264 <63975668+Simyon264@users.noreply.github.com>
2020-05-20 14:02:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,9 @@ const functions = require('./functions.js');
 const devBuild = config.info.devBuild;
 
 var prefix = config.prefix.live;
+var time = config.generalConfigPoints.timeBetweenMessages
+var lastMessage = time;
+var tickRate = config.generalConfigPoints.tickRate
 if (devBuild) prefix = config.prefix.dev;
 
 var colourInfo = config.messageColours.info;
@@ -14,23 +17,33 @@ var colourWarn = config.messageColours.warn;
 
 client.once('ready', () => {
 	console.log('Ready');
+	setInterval(timer, tickRate)
 });
 
+function timer(){
+lastMessage = lastMessage - 10
+if(lastMessage > -1000){
+if(config.generalConfigPoints.logging === true){
+}}}
+
 client.on('message', (message) => {
+
 	if (!message.guild) return;
-	
+	if (message.author.bot) return;
 	var content = message.content;
-	
+
 	if (content.substring(0, prefix.length).toLowerCase() == prefix.toLowerCase()) {
 		var args = content.substring(prefix.length).split(" ");
-		
+		if(args[0] == "time"){
+		if(config.generalConfigPoints.announcePoints = true){
+		functions.embed(message.channel,"time",colourInfo,`${lastMessage}`)
+		}
+		return;
+		}
 		fs.stat(`./commands/${args[0]}.js`, function(err, stat) {
 			if (err == null) {
 				try {
 					var commandFile = require(`./commands/${args[0]}`);
-					// Message: The entire message object
-					// Prefix: the prefix configured
-					// Args: The string containing the command argument array, including the command name
 					commandFile['run'](message, prefix, args);
 				} catch(err) {
 					console.log(`ERROR: Couldn't find command file.`);
@@ -47,6 +60,38 @@ client.on('message', (message) => {
 				functions.embed(message.channel, "", colourWarn, "An unexpected error has occured. Please contact the bot owner");
 			}
 		});
+	}else{
+		if(lastMessage < 0){
+			var user;
+			var dbTickets = fs.readFileSync('./points.json');
+			var tableTickets = JSON.parse(dbTickets);
+			for (let i = 0; i < tableTickets.length; i++) {
+				if (tableTickets[i].tag == message.author.tag) {
+					user = tableTickets[i];
+			}}
+			if (user == null) {
+				tableTickets.push({
+					"tag": message.author.tag,
+					"points": 1
+				});
+			} else {
+				for (let i = 0; i < tableTickets.length; i++) {
+					if (tableTickets[i].tag == message.author.tag) {
+						tableTickets[i].points++;
+					}
+				}
+			}
+			if(config.generalConfigPoints.logging == true){
+			console.log("Reset")
+			}
+			lastMessage = config.generalConfigPoints.timeBetweenMessages;
+			setInterval(timer, tickRate)
+	
+			fs.writeFileSync('./points.json', JSON.stringify(tableTickets));
+			if(config.generalConfigPoints.announcePoints === true){
+			functions.embed(message.channel, "+1 Point", colourInfo, message.author.tag);
+			}
+		}
 	}
 });
 
@@ -68,7 +113,3 @@ if (devBuild) {
 } else {
 	client.login(config.tokens.live);
 }
-
-process.on('unhandledRejection', (err) => {
-	console.error(err);
-});
