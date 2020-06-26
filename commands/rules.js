a4c21f99188ae465010faabe88614d9b901ece6a
Ethan Lane <elane2501@gmail.com>
2020-06-26 15:53:53 +0100

    Text boxes for rules

--- commands/rules.js
+++ commands/rules.js
@@ -1,5 +1,6 @@
 const config = require('../config.json');
 const functions = require('../functions.js');
+const fs = require('fs');
 
 var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
@@ -11,6 +12,19 @@ module.exports = {
 	usage: '',
 	roles: 'vylpes',
 	run: function(message, prefix, args) {
-		
+		if (fs.existsSync("rules.txt")) {
+			var rulesText = fs.readFileSync('rules.txt').toString();
+			rulesText = rulesText.split("> ");
+
+			for (let i = 1; i < rulesText.length; i++) {
+				let rulesLines = rulesText[i].split("\n");
+				let rulesTitle = rulesLines[0];
+				let rulesDescription = rulesLines.slice(1).join("\n");
+
+				functions.embed(message.channel, rulesTitle, colourInfo, rulesDescription);
+			}
+		} else {
+			functions.embed(message.channel, "Error", colourWarn, "rules.txt doesn't exist");
+		}
 	}
 }
