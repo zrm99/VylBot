f4365d7fca27e4c44fcd0c12dd7185aea0f51345
Ethan Lane <elane2501@gmail.com>
2020-06-26 15:30:51 +0100

    Rules command: Parameters if statement

--- commands/rules.js
+++ commands/rules.js
@@ -8,11 +8,19 @@ module.exports = {
 	name: 'rules',
 	description: 'Server rules management',
 	category: 'owner',
-	usage: '<File|Channel>',
+	usage: '<file|channel>\n- file: Generates sample rules text file\n- channel: Posts rules.txt to the rules channel',
 	roles: 'vylpes',
 	run: function(message, prefix, args) {
 		if (args.length == 1) {
-			functions.embed(message.channel, "Rules", colourInfo, "Options: File, Channel");
+			functions.embed(message.channel, "Rules", colourInfo, "Options: file, channel");
+		} else {
+			if (args[1] == "file") {
+				// TODO: Generate sample text file here
+			} else if (args[1] == "channel") {
+				// TODO: Read text file (if exists) and post to rules channel here
+			} else {
+				functions.embed(message.channel, "Incorrect usage", colourWarn, "The parameters should have been either `file` or `channel`");
+			}
 		}
 	}
 }
