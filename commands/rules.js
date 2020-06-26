4f884a2e084d6d96c254e656c562fd67124bd1d0
Ethan Lane <elane2501@gmail.com>
2020-06-26 15:33:07 +0100

    Reset rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -6,21 +6,11 @@ var colourWarn = config.messageColours.warn;
 
 module.exports = {
 	name: 'rules',
-	description: 'Server rules management',
+	description: 'Generates the rules embeds from the rules.txt file',
 	category: 'owner',
-	usage: '<file|channel>\n- file: Generates sample rules text file\n- channel: Posts rules.txt to the rules channel',
+	usage: '',
 	roles: 'vylpes',
 	run: function(message, prefix, args) {
-		if (args.length == 1) {
-			functions.embed(message.channel, "Rules", colourInfo, "Options: file, channel");
-		} else {
-			if (args[1] == "file") {
-				// TODO: Generate sample text file here
-			} else if (args[1] == "channel") {
-				// TODO: Read text file (if exists) and post to rules channel here
-			} else {
-				functions.embed(message.channel, "Incorrect usage", colourWarn, "The parameters should have been either `file` or `channel`");
-			}
-		}
+		
 	}
 }
