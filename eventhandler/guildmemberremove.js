5c9d335192b6740fee28b0d1967f5381efefb611
Ethan Lane <elane2501@gmail.com>
2020-09-07 11:45:17 +0100

    Improved guildMemberRemove embed

--- eventhandler/guildmemberremove.js
+++ eventhandler/guildmemberremove.js
@@ -1,5 +1,6 @@
 const config = require('../config.json');
 const functions = require("../functions.js")
+const discord = require('discord.js');
 
 var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
@@ -7,7 +8,15 @@ var colourWarn = config.messageColours.warn;
 module.exports = {
     run: function(client) {
         client.on('guildMemberRemove', (member) => {
-            functions.embed(member.guild.channels.find(channel => channel.name == config.channels.logging), "Member left/kicked/banned", colourInfo, `Member: ${member.user.tag}\nID: ${member.id}`);
+            var embed = new discord.RichEmbed()
+                .setTitle("Member Left")
+                .setColor(colourInfo)
+                .addField("User", `${member} \`${member.user.tag}\``)
+                .addField("Joined", `${member.joinedAt}`)
+                .setFooter(`User ID: ${member.user.id}`)
+                .setThumbnail(member.user.displayAvatarURL);
+                
+            member.guild.channels.find(channel => channel.name == config.channels.logging).send(embed);
         });
     }
 }
