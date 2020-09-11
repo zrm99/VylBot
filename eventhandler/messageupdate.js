d1ff4fd7b0ff8b52373252e4f50ab9ddbe28ee24
Ethan Lane <elane2501@gmail.com>
2020-09-11 11:55:21 +0100

    Made messageupdate embed look nicer

--- eventhandler/messageupdate.js
+++ eventhandler/messageupdate.js
@@ -1,5 +1,6 @@
 const config = require('../config.json');
-const functions = require("../functions.js")
+const functions = require("../functions.js");
+const discord = require("discord.js");
 
 var colourInfo = config.messageColours.info;
 var colourWarn = config.messageColours.warn;
@@ -9,8 +10,17 @@ module.exports = {
         client.on('messageUpdate', (oldMessage, newMessage) => {
             if (newMessage.author.bot) return;
             if (oldMessage.content == newMessage.content) return;
-            
-            functions.embed(newMessage.guild.channels.find(channel => channel.name == config.channels.logging), "Message Edited", colourInfo, `Member: ${newMessage.author.tag}\nOld: ${oldMessage.content}\nNew: ${newMessage.content}\nChannel: ${newMessage.channel}`);
+
+            var embed = new discord.RichEmbed()
+                .setTitle("Message Edited")
+                .setColor(colourInfo)
+                .addField("User", `${newMessage.author} \`${newMessage.author.tag}\``)
+                .addField("Channel", newMessage.channel)
+                .addField("Old Content", `\`\`\`${oldMessage.content || "*none*"}\`\`\``)
+                .addField("New Content", `\`\`\`${newMessage.content || "*none*"}\`\`\``)
+                .setThumbnail(newMessage.author.displayAvatarURL);
+                
+            newMessage.guild.channels.find(channel => channel.name == config.channels.logging).send(embed);
         });
     }
 }
