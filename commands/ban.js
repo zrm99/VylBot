adf3ce721380bbc8809f06fbd2eda7aca3b091b7
Ethan Lane <elane2501@gmail.com>
2020-09-22 23:08:34 +0100

    Rewrote bot.js command, made the bot now check if the bot can ban before it does

--- commands/ban.js
+++ commands/ban.js
@@ -12,70 +12,54 @@ module.exports = {
 	category: 'moderation',
 	usage: '<user> [reason]',
 	roles: 'Moderator',
-	run: function(message, prefix, args) {
+	run: function (message, prefix, args) {
 		if (message.member.roles.find(role => role.name == config.roles.moderator)) {
-			var user = message.mentions.users.first();
-			var serverName = message.guild.name;
-			if (user) {
-				var member = message.guild.member(user);
-		
-				if (member) {
-					var argsReason = args;
-					argsReason.splice(0, 2);
-		
-					var reason = argsReason.join(" ");
-					if(message.guild.available){
-						var embed = new discord.RichEmbed()
-								.setTitle(`You were banned from: ${serverName}`)
-								.setColor(colourWarn)
-								.setDescription(`For the reason: ${reason}`);
-						member.send(embed).then(()=>{
-							member.ban().then(() => {
-								functions.embed(message.channel, "", colourInfo, user.tag + " has been banned");
-								
-								let embed = new discord.RichEmbed()
-									.setTitle("Member Banned")
-									.setColor(colourMod)
-									.addField("User", `${user} \`${user.tag}\``)
-									.addField("Moderator", `${message.author} \`${message.author.tag}\``)
-									.addField("Reason", reason || "*none*")
-									.setThumbnail(user.displayAvatarURL);
-									
-								message.guild.channels.find(channel => channel.name == config.channels.logging).send(embed);
-							}).catch(() => {
-								functions.embed(message.channel, "", colourWarn, "There was an error banning this user, maybe I'm missing permissions?");
-							});
-						}).catch(() => {
-							member.ban().then(() => {
-								functions.embed(message.channel, "", colourInfo, user.tag + " has been banned");
+			let user = message.mentions.users.first();
+			let serverName = message.guild.name;
 
-								let embed = new discord.RichEmbed()
-									.setTitle("Member Banned")
-									.setColor(colourMod)
-									.addField("User", `${user} \`${user.tag}\``)
-									.addField("Moderator", `${message.author} \`${message.author.tag}\``)
-									.addField("Reason", reason || "*none*")
-									.setThumbnail(user.displayAvatarURL);
-									
-								message.guild.channels.find(channel => channel.name == config.channels.logging).send(embed);
+			if (user) {
+				let member = message.guild.member(user);
+
+				if (member) {
+					let reason = args;
+					reason.splice(0, 2);
+					reason.join(" ");
+
+					if (message.guild.available) {
+						if (member.bannable) {
+							let embedLog = new discord.RichEmbed()
+								.setTitle("Member Banned")
+								.setColor(colourMod)
+								.addField("User", `${user} \`${user.tag}\``)
+								.addField("Moderator", `${message.author} \`${message.author.tag}\``)
+								.addField("Reason", reason || "*none*")
+								.setThumbnail(user.displayAvatarURL);
+
+							let embedPublic = new discord.RichEmbed()
+								.setColor(colourInfo)
+								.setDescription(`${user.tag} has been banned`);
+
+							member.ban(reason).then(() => {
+								message.guild.channels.find(channel => channel.name == config.channels.logging).send(embedLog);
+								message.channel.send(embedPublic);
 							}).catch(() => {
-								functions.embed(message.channel, "", colourWarn, "There was an error banning this user, maybe I'm missing permissions?");
+								let embedError = new discord.RichEmbed()
+									.setColor(colourWarn)
+									.setDescription("An error has occured");
+
+								message.channel.send(embedError);
 							});
-						});
-					}else{
-					functions.embed(message.channel, "Err", colourWarn, "An unkown error occurerd")
+						} else {
+							let embedError = new discord.RichEmbed()
+								.setColor(colourWarn)
+								.setDescription("I am unable to ban this user. Am I missing permissions?");
+
+							message.channel.send(embedError);
+						}
 					}
-				} else {
-					functions.embed(message.channel, "", colourWarn, "This user is not in the server");
 				}
-			} else {
-				functions.embed(message.channel, "", colourWarn, "Please specify a user by mentioning them");
 			}
-		} else {
-			functions.embed(message.channel, "", colourWarn, "You do not have permission to run this command");
 		}
-	
-		message.delete();
 	}
-	
+
 }
